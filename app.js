e56c1a489ebfe8f6b46c05887c082d801caea927
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-05-20 14:48:23 +0100

    handle delete

--- app.js
+++ app.js
@@ -82,6 +82,19 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     },
   });
 });
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      massage: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
 //This is how we send different responses for different http methods request
 const port = 3000;
