8a86900a6fbef160027631ae81eb7f78f77df81b
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-05-20 13:41:10 +0100

    handle tour post request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Hello from the server', app: 'Natours' });
@@ -22,6 +23,28 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
 });
 
 //This is how we send different responses for different http methods request
