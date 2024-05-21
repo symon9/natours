39bff654948a7a986bbe1d2eba3c9e5e151e3a4f
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-05-21 16:46:19 +0100

    serving a static file

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 // 1) MIDDLEWARES STACK FOR ALL ROUTES
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from middle');
