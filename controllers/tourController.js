49a53f09639a96c2b04b98ac532173f020c2dab6
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-05-24 09:13:40 +0100

    model the tour data

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -49,7 +49,7 @@ exports.createTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: 'Fail',
-      message: 'Invalid data sent!',
+      message: error,
     });
   }
 };
