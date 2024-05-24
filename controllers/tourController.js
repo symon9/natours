ab19ffbe198d2c09dd9a76c22e3ab231a9221d76
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-05-24 23:29:18 +0100

    implement limiting fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,6 +25,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    //3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields); // including
+    } else {
+      query = query.select('-__v'); // excluding
+    }
+
     // EXECUTE THE QUERY
     const tours = await query;
 
