d2a2e5624fdb264f12cb7a0cf06801b77ffd7e5b
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-05-28 11:33:43 +0100

    add custom validator

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator'); //TODO: npm i validator
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equal than 40 character'],
       minlength: [40, 'A tour name must have more or equal than 10 character'],
+      //validate: [validator.isAlpha, 'Tour name must only contains character'],
     },
     slug: String,
     duration: {
@@ -39,7 +41,16 @@ const tourSchema = new mongoose.Schema(
       default: 0,
     },
     price: { type: Number, required: [true, 'A tour must have a price'] },
-    discountPrice: Number,
+    discountPrice: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on new document creation.
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
