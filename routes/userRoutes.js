ad7ed9020b079f32f26a670c6f9cf2512ab008b8
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-05-29 15:41:31 +0100

    implement new to create new user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-/* USERS */
+router.post('/signup', authController.signup);
+
 router
   .route('/')
   .get(userController.getAllUsers)
