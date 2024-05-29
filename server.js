7aa5f30695776f9311bcc01ce5bc72a9f8bdbea3
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-05-29 13:17:24 +0100

    handle rejections

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ mongoose
 const app = require('./app');
 
 const port = process.env.PORT || 3000; // else use 3000
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection: Shutting dowm...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
