ca1901ee0ef4d7e561adacffb39632f636a6ebe9
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-05-29 13:32:29 +0100

    catch uncaught exception.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({
-  path: './config.env',
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exception: Shutting dowm...');
+  console.log(err.name, err.message);
+  process.exit(1);
 });
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -18,16 +23,14 @@ mongoose
   })
   .then(() => console.log('DB connection successfully'));
 
-const app = require('./app');
-
 const port = process.env.PORT || 3000; // else use 3000
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('Unhandled Rejection: Shutting dowm...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
