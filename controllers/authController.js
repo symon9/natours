0a095ece5f60bb31856d8cdc7be286b4bf7cda1f
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-05-31 23:17:12 +0100

    create sign up for users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,25 @@
+const jwt = require('jsonwebtoken'); //TODO: npm i jsonwebtoken
 const User = require('./../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
-  console.log('Uncaught Exception: Shutting dowm...');
+  console.log('Uncaught Exception: Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
@@ -29,7 +29,7 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log('Unhandled Rejection: Shutting dowm...');
+  console.log('Unhandled Rejection: Shutting down...');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
