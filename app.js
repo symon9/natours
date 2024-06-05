1ecd78a8624ad8c4599b92e774ad448db01743d0
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-06-05 23:04:09 +0100

    implement rate limit to 100 request per hour

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,10 +9,17 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES STACK FOR ALL ROUTES
+// 1) GLOBAL MIDDLEWARES STACK FOR ALL ROUTES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, Please try again in an hour!',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
