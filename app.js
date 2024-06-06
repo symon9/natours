5997b08a3e182ed9632d97a4d4a7540f1928a25d
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-06-06 13:48:42 +0100

    install helmet and express-rate-limit

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.static(`${__dirname}/public`));
 
 //Test middleware
 app.use((req, res, next) => {
-  req.requestTime = newDate().toISOString();
+  req.requestTime = new Date().toISOString();
   //console.log(req.headers)
   next();
 });
