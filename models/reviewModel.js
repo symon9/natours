45586b5e113e06eb17e7426a3e7a7c267a277649
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-06-08 14:09:44 +0100

    populate review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -36,6 +36,18 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+// QUERY MIDDLEWARE
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
