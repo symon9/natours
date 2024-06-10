990b77fc90e3fbcbc74bf2d61d7955831c999572
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-06-10 20:08:45 +0100

    extends our base template with blocks

--- app.js
+++ app.js
@@ -76,6 +76,18 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/overview', (req,res) => {
+  res.status(200).render('overview', {
+    title: 'All tours'
+  })
+});
+
+app.get('/tour', (req,res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour'
+  })
+});
+
 //mount the router
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
