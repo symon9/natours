68e4b6f34d232b91e7cd3437a551f5e276024d10
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-06-11 19:48:07 +0100

    build the tour page without map section yet

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const AppError = require('../utils/AppError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
@@ -13,8 +14,21 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res,next) => {
+  // 1) get tour data for requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+
+  if (!tour) {
+    next(new AppError('No document found', 404));
+  }
+  // 2) Build template
+
+  // 3) Render that template using tour data from 1
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour,
+  });
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,6 @@ const viewsController = require('../controllers/viewsController');
 const router = express.Router();
 
 router.get('/', viewsController.getOverview);
-router.get('/tour', viewsController.getOverview);
+router.get('/tour/:slug', viewsController.getOverview);
 
 module.exports = router;
