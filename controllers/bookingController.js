36e14ee55d7c1d99ccfe83a5654972362f07f37a
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-06-21 17:58:19 +0100

    finish the CRUD operations for the bookings api

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -43,3 +43,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
